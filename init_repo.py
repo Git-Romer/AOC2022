96c32838772c03508e8fd0c2272223e9b139f4b8
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-09-12 03:11:04 +0200

    added file to each folder at init

--- init_repo.py
+++ init_repo.py
@@ -11,3 +11,5 @@ for user in users: # Creating directories for specific users
 for folder in range(1,26): # Creating folders for each day for each user
     for user in users:
         os.mkdir(os.path.join('./' + user, 'Day_' + str(folder)))
+        with open('./' + user + '/Day_' + str(folder) + '/main.py', 'w') as f:
+            f.write('# Day {}'.format(str(folder)))
