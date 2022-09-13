680f133eee4eb46ac822e6caaa66291104a74764
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-09-13 05:50:02 +0200

    adding pythonanywhere to trusted hosts

--- AOC_scoreboard/AOC_scoreboard/settings.py
+++ AOC_scoreboard/AOC_scoreboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,9 @@ SECRET_KEY = 'django-insecure-6t-sq8h2kj&oeqzmh7nt-w4!nwwx&tyj&^%$o9xdz&w@m(ol20
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "gromer.pythonanywhere.com",
+]
 
 
 # Application definition
@@ -117,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
