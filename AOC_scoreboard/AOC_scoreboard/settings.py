bf4f6077de8f0e73463fdb5cddfac2e50233623e
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-09-16 01:57:03 +0200

    exported and created new secret

--- AOC_scoreboard/AOC_scoreboard/settings.py
+++ AOC_scoreboard/AOC_scoreboard/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from .django_secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6t-sq8h2kj&oeqzmh7nt-w4!nwwx&tyj&^%$o9xdz&w@m(ol20'
+SECRET_KEY = DEV_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
