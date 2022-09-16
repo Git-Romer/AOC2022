f95921508cc08bae5ab979167ca0e7d890314051
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-09-16 04:38:40 +0200

    fixed pythonanywhere err when trying to update db

--- AOC_scoreboard/scoreboard_updater/updater.py
+++ AOC_scoreboard/scoreboard_updater/updater.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from scoreboard_updater import scoreboard_api
+import socket
 
 def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(scoreboard_api.update, 'interval', minutes=15)
-    scheduler.start()
+    if socket.gethostbyname(socket.gethostname()) == '127.0.0.1':
+        print(f"Server launched on {socket.gethostbyname(socket.gethostname())}\nReocurring scoreboard update will not be executed!")
+    else:
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(scoreboard_api.update, 'interval', minutes=15)
+        scheduler.start()
