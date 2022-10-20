bdde5fe27032d1fd894409c3b148f28a7cf17a31
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-10-20 03:18:07 +0200

    replaced ip condition with debug mode

--- AOC_scoreboard/scoreboard_updater/updater.py
+++ AOC_scoreboard/scoreboard_updater/updater.py
@@ -2,10 +2,11 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from scoreboard_updater import scoreboard_api
 import socket
+from django.conf import settings
 
 def start():
-    if socket.gethostbyname(socket.gethostname()) == '127.0.0.1':
-        print(f"Server launched on {socket.gethostbyname(socket.gethostname())}\nReocurring scoreboard update will not be executed!")
+    if settings.DEBUG:
+        print(f"Server launched on {socket.gethostbyname(socket.gethostname())} and Debug mode is on\nReocurring scoreboard update will not be executed!")
     else:
         scheduler = BackgroundScheduler()
         scheduler.add_job(scoreboard_api.main, 'interval', minutes=30)
