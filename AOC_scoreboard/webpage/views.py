2aa07c0d98ef3df47772b58af8234af910e3d543
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-10-20 03:16:01 +0200

    added crawler for AOC calendar

--- AOC_scoreboard/webpage/views.py
+++ AOC_scoreboard/webpage/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {})
+    # Crawling the AOC calendar from the AOC website
+    url = 'https://adventofcode.com/2022'
+    r = requests.get(url).text
+    soup = BeautifulSoup(r, 'html.parser')
+    calendar = soup.main
+    for a in calendar.find_all('a'):
+        a['href'] = 'https://adventofcode.com' + a['href']
+        a['target'] = '_blank'
+    context = {'calendar': str(calendar)}
+    return render(request, 'index.html', context)
 
 def bongocat(request):
     return render(request, 'bongo_cat.html', {})
