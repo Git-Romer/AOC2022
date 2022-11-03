88acf7506d77069adcb69ae1c39dadd299145aae
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-11-03 18:40:19 +0100

    simplified podium calculation

--- AOC_scoreboard/webpage/views.py
+++ AOC_scoreboard/webpage/views.py
@@ -22,13 +22,19 @@ def bongocat(request):
     return render(request, 'bongo_cat.html', {})
 def scoreboard(request):
     data = jsoncrawler.objects.all().order_by('-stars')
-    else:
-        thrd_best_user = ""
-    # users = jsoncrawler.objects.values_list('name', flat=True)
+    podium = {}
+    for podiums in range(3):
+        if len(data) >= podiums:
+            if not data.order_by('-stars', 'name').values('stars')[podiums].get('stars') == 0:
+                podium[podiums] = data.order_by('-stars', 'name')[podiums]
+            else:
+                podium[podiums] = ""
+        else:
+            podium[podiums] = ""
     context = {
         'data': data,
-        'best_user': best_user,
-        'snd_best_user': snd_best_user,
-        'thrd_best_user': thrd_best_user,
+        'best_user': podium[0],
+        'snd_best_user': podium[1],
+        'thrd_best_user': podium[2],
     }
     return render(request, 'scoreboard.html', context)
