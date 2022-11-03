3aca20f2134d7ab74a565792d32c0dab29bc7241
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-11-04 00:47:14 +0100

    added stats view to populate chart

--- AOC_scoreboard/webpage/views.py
+++ AOC_scoreboard/webpage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-import requests, datetime
+from django.db.models.functions import Lower
+import requests, datetime, numpy as np, seaborn as sns
 from bs4 import BeautifulSoup
 # Importing Table
 from scoreboard.models import jsoncrawler
@@ -19,12 +20,12 @@ def index(request):
     return render(request, 'index.html', context)
 
 def scoreboard(request):
-    data = jsoncrawler.objects.all().order_by('-stars')
+    data = jsoncrawler.objects.all()
     podium = {}
     for podiums in range(3):
         if len(data) >= podiums:
-            if not data.order_by('-stars', 'name').values('stars')[podiums].get('stars') == 0:
-                podium[podiums] = data.order_by('-stars', 'name')[podiums]
+            if not data.order_by('-stars', Lower('name')).values('stars')[podiums].get('stars') == 0:
+                podium[podiums] = data.order_by('-stars', Lower('name'))[podiums]
             else:
                 podium[podiums] = ""
         else:
@@ -49,8 +50,50 @@ def scoreboard(request):
     return render(request, 'scoreboard.html', context)
 
 def stats(request):
-    return render(request, 'stats.html', {})
+    def colorize(x):
+        randcolor = []
+        color_list_hex = sns.color_palette("pastel", x).as_hex()
+        for hex in color_list_hex:
+            randcolor.append(list(int(hex.lstrip('#')[i:i+2], 16) for i in (0, 2, 4)))
+        return randcolor
 
+    def randomdata():
+        randdata = sorted(list(np.random.choice(range(50), size=25)))
+        randdata[0] = 0
+        return randdata
+    def randomday():
+        randday = list(range(np.random.choice(range(2,25))))
+        return randday
+
+    data = jsoncrawler.objects.all()
+    
+    if datetime.datetime.now().date() >= datetime.date(datetime.datetime.now().year, 12, 1) and datetime.datetime.now().date() <= datetime.date(datetime.datetime.now().year, 12, 25):
+        max_day = datetime.datetime.now().day
+        max_stars = 2 * max_day
+    else:
+        max_day = 0
+        max_stars = 0
+
+    dataset = ""
+    colorpalette = colorize(len(data))
+    for i, user in enumerate(data.order_by(Lower('name'))):
+        dataset = dataset + \
+        f"""
+        {{
+            label: "{user.name}",
+            backgroundColor: 'rgb({', '.join(map(str, colorpalette[i]))})',
+            borderColor: 'rgb({', '.join(map(str, colorpalette[i]))})',
+            data: {randomdata()},
+            tension: 0.2,
+        }},"""
+
+    context = {
+        "dataset": dataset,
+        "randomday": randomday(),
+        "max_day": max_day,
+        "max_stars": max_stars,
+    }
+    return render(request, 'stats.html', context)
 
 def bongocat(request):
     return render(request, 'bongo_cat.html', {})
