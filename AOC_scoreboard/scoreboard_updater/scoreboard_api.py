7c98cec087e1c49fb49e8f467353d3cfe575d7c9
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-11-05 01:01:29 +0100

    added error catch in case api key or url is wrong

--- AOC_scoreboard/scoreboard_updater/scoreboard_api.py
+++ AOC_scoreboard/scoreboard_updater/scoreboard_api.py
@@ -11,29 +11,34 @@ def main():
     def update():
 
         response = get_scoreboard_json()
+        if response.status_code == 200 and response.headers['Content-Type'] == 'application/json':
+            response = response.json()
+            for member in response["members"].keys():
+                member_id = response["members"][member]["id"]
+                name = response["members"][member]["name"]
+                stars = response["members"][member]["stars"]
+                global_score = response["members"][member]["global_score"]
+                local_score = response["members"][member]["local_score"]
+                last_star_ts = response["members"][member]["last_star_ts"]
+                completion_day_level = response["members"][member]["completion_day_level"]
+                event = response["event"]
 
-        for member in response["members"].keys():
-            member_id = response["members"][member]["id"]
-            name = response["members"][member]["name"]
-            stars = response["members"][member]["stars"]
-            global_score = response["members"][member]["global_score"]
-            local_score = response["members"][member]["local_score"]
-            last_star_ts = response["members"][member]["last_star_ts"]
-            completion_day_level = response["members"][member]["completion_day_level"]
-            event = response["event"]
-
-            res = jsoncrawler(
-                member_id=member_id,
-                name=name,
-                stars=stars,
-                global_score=global_score,
-                local_score=local_score,
-                last_star_ts=last_star_ts,
-                completion_day_level=completion_day_level,
-                event=event
-            )
-            res.save()
-        print("Updated scoreboard!")
+                res = jsoncrawler(
+                    member_id=member_id,
+                    name=name,
+                    stars=stars,
+                    global_score=global_score,
+                    local_score=local_score,
+                    last_star_ts=last_star_ts,
+                    completion_day_level=completion_day_level,
+                    event=event
+                )
+                res.save()
+            print("Updated scoreboard!")
+        elif response.headers['Content-Type'] == 'text/html':
+            print("Error: Response is not JSON")
+        else:
+            print(f"/api/alerts response: {response.status_code}: {response.reason} \n{response.content}")
 
     update()
 
