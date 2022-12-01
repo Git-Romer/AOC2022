554c94f9e47cbb1a05f733a7fba4b73692046729
Maroko1218 <markus.svhn@gmail.com>
2022-12-01 06:04:47 +0100

    Part 1 solve

--- markus/Day_1/main.py
+++ markus/Day_1/main.py
@@ -1,1 +1,9 @@
 # Day 1
+puzzle = ""
+with open("puzzle_input.txt") as file:
+    puzzle = file.read().split("\n\n")
+
+elves = []
+for entry in puzzle:
+    elves.append(sum(int(x) for x in entry.split("\n")))
+print(max(elves))
