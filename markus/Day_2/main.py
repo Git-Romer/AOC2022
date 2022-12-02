2e63da4b99bd278aa17087905c4f5fcbf345e5c1
Maroko1218 <markus.svhn@gmail.com>
2022-12-02 06:14:45 +0100

    Day 2 Part 1 Solve

--- markus/Day_2/main.py
+++ markus/Day_2/main.py
@@ -1,1 +1,34 @@
 # Day 2
+#A X ROCK 
+#B Y PAPER
+#C Z SCISSOR
+
+puzzle = ""
+with open("puzzle_input.txt") as file:
+    puzzle = file.read().split("\n")
+for i in range(len(puzzle)):
+    puzzle[i] = puzzle[i].replace("X", "A")
+    puzzle[i] = puzzle[i].replace("Y", "B")
+    puzzle[i] = puzzle[i].replace("Z", "C")
+
+score = 0
+for match in puzzle:
+    if match[0] == match[2]:
+        score += 3
+
+    if match[2] == "A":
+        score += 1
+        if match[0] == "C":
+            score += 6
+        
+    elif match[2] == "B":
+        score += 2
+        if match[0] == "A":
+            score += 6
+    
+    elif match[2] == "C":
+        score += 3
+        if match[0] == "B":
+            score += 6
+
+print(score)
