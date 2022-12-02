9c38128fd78e458f6fc4cfdcaca7bf5508e99599
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-12-02 15:59:20 +0100

    simplified part 1

--- gilles/Day_1/main.py
+++ gilles/Day_1/main.py
@@ -4,38 +4,48 @@
 import os
 path = os.path.dirname(__file__)
 
-# Load data from txt file
-dataset = open(os.path.join(path, 'food_supplies.txt'), 'r').readlines()
-
 # Functions
-def create_elf_db(data):
-    elfcount = 0
-    dataset = {}
-    for line in data:
-        line = line.replace('\n', '')
-        elf = f"elf{elfcount + 1}"
-        if not line == '':
-            if elf in dataset:
-                dataset[elf]["calories"].append(line)
-            else:
-                dataset[elf] = {"calories": [line]}
-        else:
-            elfcount += 1
-            dataset[elf]["sum"] = sum([int(x) for x in dataset[elf]["calories"]])
-    dataset[elf]["sum"] = sum([int(x) for x in dataset[elf]["calories"]])
+# Load data from txt file
+def load_data():
+    with open(os.path.join(path, 'food_supplies.txt'), 'r') as f:
+        dataset = f.read()
     return dataset
 
-def get_highest_value(dataset):
+# Create database
+def create_elf_db(data):
+    # Split at every double line break
+    elf_db = data.split('\n\n')
+    # Split at every line break
+    elf_db = [x.split('\n') for x in elf_db]
+    # Remove empty strings in list of lists
+    elf_db = [[y for y in x if y] for x in elf_db]
+    # Convert list of lists to dictionary with category "calories"
+    elf_db = {f"elf{elf_db.index(x) + 1}": {"calories": x} for x in elf_db}
+    return elf_db
+
+# Calculate sum of calories
+def get_calories(elf_db):
+    for elf in elf_db:
+        # Sum of calories
+        elf_db[elf]["sum"] = sum([int(x) for x in elf_db[elf]["calories"]])
+    return elf_db
+
+# Get highest value
+def get_highest_value(elf_db):
     highest_value = 0
-    for elf in dataset:
-        if dataset[elf]["sum"] > highest_value:
-            highest_value = dataset[elf]["sum"]
+    for elf in elf_db:
+        if elf_db[elf]["sum"] > highest_value:
+            highest_value = elf_db[elf]["sum"]
     return highest_value
 
 # Main
 if __name__ == "__main__":
+    # Load data
+    dataset = load_data()
     # Create database
     elf_db = create_elf_db(dataset)
+    # Get calories
+    elf_db = get_calories(elf_db)
     # Get highest value
     highest_value = get_highest_value(elf_db)
     # Print result
