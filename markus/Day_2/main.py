d63fc6d2c716fb1ba62b12d0abaf6abc3aaa512f
Maroko1218 <markus.svhn@gmail.com>
2022-12-02 06:22:15 +0100

    Day 2 Part 2 The most unclean solve.

--- markus/Day_2/main.py
+++ markus/Day_2/main.py
@@ -1,34 +1,40 @@
 # Day 2
-#A X ROCK 
-#B Y PAPER
-#C Z SCISSOR
+#A ROCK 
+#B PAPER
+#C SCISSOR
+#X WIN
+#Y LOSE
+#Z DRAW
+
 
 puzzle = ""
 with open("puzzle_input.txt") as file:
     puzzle = file.read().split("\n")
-for i in range(len(puzzle)):
-    puzzle[i] = puzzle[i].replace("X", "A")
-    puzzle[i] = puzzle[i].replace("Y", "B")
-    puzzle[i] = puzzle[i].replace("Z", "C")
 
 score = 0
 for match in puzzle:
-    if match[0] == match[2]:
+    if "Y" in match:
         score += 3
-
-    if match[2] == "A":
-        score += 1
-        if match[0] == "C":
-            score += 6
-        
-    elif match[2] == "B":
-        score += 2
-        if match[0] == "A":
-            score += 6
-    
-    elif match[2] == "C":
-        score += 3
-        if match[0] == "B":
-            score += 6
+        if "A" in match:
+            score += 1
+        elif "B" in match:
+            score += 2
+        elif "C" in match:
+            score += 3
+    elif "Z" in match:
+        score += 6
+        if "A" in match:
+            score += 2
+        elif "B" in match:
+            score += 3
+        elif "C" in match:
+            score += 1
+    else:
+        if "A" in match:
+            score += 3
+        elif "B" in match:
+            score += 1
+        elif "C" in match:
+            score += 2
 
 print(score)
