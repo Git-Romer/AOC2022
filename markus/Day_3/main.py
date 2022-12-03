28fda6761f16461de7e4fc91948f8d82a4a89c59
Maroko1218 <markus.svhn@gmail.com>
2022-12-03 06:44:44 +0100

    Day 3 Part 2 Solve

--- markus/Day_3/main.py
+++ markus/Day_3/main.py
@@ -3,15 +3,13 @@ puzzle = []
 with open("puzzle_input.txt") as file:
     puzzle = file.read().split("\n")
 
-
 duplicates = []
-for sack in puzzle:
-    left = slice(0, len(sack)//2)
-    right = slice(len(sack)//2, len(sack))
-    for char in sack[left]:
-        if char in sack[right]:
+for i in range(0, len(puzzle), 3):
+    for char in puzzle[i]:
+        if char in puzzle[i+1] and char in puzzle[i+2]:
             duplicates.append(char)
             break
+    
 total = 0
 for char in duplicates:
     if char.isupper():
