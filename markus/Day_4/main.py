bb324f377b998a17bb7e2d27d58eff791553da5b
Maroko1218 <markus.svhn@gmail.com>
2022-12-04 17:58:40 +0100

    day 4 part 2 solve

--- markus/Day_4/main.py
+++ markus/Day_4/main.py
@@ -8,12 +8,14 @@ for pair in puzzle:
     temp = pair.split(",")
     ll, lh = temp[0].split("-")
     rl, rh = temp[1].split("-")
+    ll, lh, rl, rh = int(ll), int(lh), int(rl), int(rh)
     #PERFECT CODE ABOVE
     #INSANE CODE BELOW
-
-    if int(ll) <= int(rl) and int(lh) >= int(rh):
-        total += 1
-    elif int(ll) >= int(rl) and int(lh) <= int(rh):
-        total += 1
-
+    list1 = range(ll, lh+1)
+    list2 = range(rl, rh+1)
+    for num in list1:
+        if num in list2:
+            total+=1
+            break
+        
 print(total)
