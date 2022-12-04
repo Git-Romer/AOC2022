7b411c6fd0395d8bf6d5682160a79fc5296d9623
Maroko1218 <markus.svhn@gmail.com>
2022-12-04 17:44:02 +0100

    Day 4 Part 1 solve

--- markus/Day_4/main.py
+++ markus/Day_4/main.py
@@ -1,1 +1,19 @@
 # Day 4
+puzzle = []
+with open("puzzle_input.txt") as file:
+    puzzle = file.read().split("\n")
+
+total = 0
+for pair in puzzle:
+    temp = pair.split(",")
+    ll, lh = temp[0].split("-")
+    rl, rh = temp[1].split("-")
+    #PERFECT CODE ABOVE
+    #INSANE CODE BELOW
+
+    if int(ll) <= int(rl) and int(lh) >= int(rh):
+        total += 1
+    elif int(ll) >= int(rl) and int(lh) <= int(rh):
+        total += 1
+
+print(total)
