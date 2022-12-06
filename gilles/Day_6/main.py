1e2e1238c2376a840fdbbd1d2e5fd834b5c87226
Git-Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2022-12-06 08:41:36 +0100

    part 2

--- gilles/Day_6/main.py
+++ gilles/Day_6/main.py
@@ -12,19 +12,22 @@ def loader_data():
         dataset = f.read()
     return dataset
 
-# Check when 4 characters in list are unique
-def get_start_of_packet(data):
+# Check when x characters in list are unique
+def get_start_of_packet(data, length_of_packet):
     packet_group = []
     for i in range(len(data)):
-        packet_group = data[i:i+4]
-        if len(set(packet_group)) == 4:
-            return i + 4
+        packet_group = data[i:i+length_of_packet]
+        if len(set(packet_group)) == length_of_packet:
+            return i + length_of_packet
 
 # Main
 if __name__ == '__main__':
     # Load data
     data = loader_data()
     # Check when 4 characters of strings become different
-    start_of_packet_marker = get_start_of_packet(data)
+    start_of_packet_marker = get_start_of_packet(data, 4)
+    # Check when 14 characters of strings become different
+    start_of_packet_message = get_start_of_packet(data, 14)
     # Print result
     print(f"The start-of-packet marker is at position: {start_of_packet_marker}")
+    print(f"The start-of-message marker is at position: {start_of_packet_message}")
