d6d250c5a0194720f3c6a27aed1bfe3c34f74cd6
Maroko1218 <markus.svhn@gmail.com>
2022-12-06 07:35:13 +0100

    Day 6 part 1 solve

--- markus/Day_6/main.py
+++ markus/Day_6/main.py
@@ -1,1 +1,10 @@
 # Day 6
+puzzle = ""
+with open("input.txt") as file:
+    puzzle = file.read()
+
+for i in range(3, len(puzzle)):
+    if puzzle[i-3] in (puzzle[i-2], puzzle[i-1], puzzle[i]) or puzzle[i-2] in (puzzle[i-1], puzzle[i]) or puzzle[i-1] == puzzle[i]:
+        continue
+    print(i+1)
+    break
